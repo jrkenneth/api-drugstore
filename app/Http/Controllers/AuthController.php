85247cf8d826b2d84d7c78f9c5f3cdf22e58677d
Kenneth Igharo Jr <igharoken@gmail.com>
2021-12-03 23:25:43 +0100

    sanctum fix

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -49,7 +49,7 @@ class AuthController extends Controller
 
         $user = $request->user();
         $tokenResult = $user->createToken('Personal Access Token');
-        $token = $tokenResult->token_name;
+        $token = $tokenResult->token;
         $token->expires_at = Carbon::now()->addWeeks(1);
         $token->save();
 
